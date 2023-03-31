4dfea4124af0b8c1c5f2d1010fd122934656e11d
Bron LaPoint <gemini2022@yahoo.com>
2023-03-30 20:58:11 -0400

    Saving old code

--- projects/manager/src/app/list-item.ts
+++ projects/manager/src/app/list-item.ts
@@ -14,14 +14,18 @@ export class ListItem {
     public fontFamily?: string;
     public caseType?: CaseType;
     public selectable?: boolean;
+    public hoverTextColor?: Color
     public unselectable?: boolean;
     public showSelection?: boolean;
     public selectionFillColor?: Color;
+    public selectionTextColor?: Color;
     public selectType?: ItemSelectType;
     public selectionBorderColor?: Color;
     public selectionBorderWidth?: number;
+    public secondarySelectionTextColor?: Color;
     public secondarySelectionFillColor?: Color;
     public secondarySelectionBorderColor?: Color;
+    public secondarySelectionBorderWidth?: number;
 
     constructor(itemText: string) {
         this.text = itemText;
--- projects/manager/src/app/list-options.ts
+++ projects/manager/src/app/list-options.ts
@@ -17,15 +17,22 @@ export class ListOptions {
     public scrollSnapping?: boolean = false;
     public multiselectable?: boolean = true;
     public selectionBorderWidth?: number = 1;
-    public noSelectOnArrowKey?: boolean = false;
+    public noSelectOnArrowKey?: boolean = true;
+    public secondarySelectionBorderWidth?: number = 1;
     public initiateSelectionOnArrowKey?: boolean = false;
     public textColor?: Color = new Color(160, 160, 160, 1);
     public hoverColor?: Color = new Color(255, 255, 255, 0.03);
     public fontFamily?: string = 'Arial, Helvetica, sans-serif';
+    public hoverTextColor?: Color = new Color(210, 210, 210, 1);
+    public selectionTextColor?: Color = new Color(255, 255, 255, 1);
     public selectionFillColor?: Color = new Color(255, 255, 255, 0.1);
     public selectionBorderColor?: Color = new Color(255, 255, 255, 0.85);
+    public secondarySelectionTextColor?: Color = new Color(206, 206, 206, 1);
     public secondarySelectionFillColor?: Color = new Color(255, 255, 255, 0.05);
     public secondarySelectionBorderColor?: Color = new Color(255, 255, 255, 0.25);
+
+    
+    
 
     constructor(listOptions?: ListOptions) {
         if (listOptions) {
@@ -40,6 +47,7 @@ export class ListOptions {
             if (listOptions.deletable != null) this.deletable = listOptions.deletable;
             if (listOptions.selectable != null) this.selectable = listOptions.selectable;
             if (listOptions.itemHeight != null) this.itemHeight = listOptions.itemHeight;
+            if (listOptions.hoverTextColor) this.hoverTextColor = listOptions.hoverTextColor;
             if (listOptions.itemsPerPage != null) this.itemsPerPage = listOptions.itemsPerPage;
             if (listOptions.unselectable != null) this.unselectable = listOptions.unselectable;
             if (listOptions.loopSelection != null) this.loopSelection = listOptions.loopSelection;
@@ -48,12 +56,15 @@ export class ListOptions {
             if (listOptions.scrollSnapping != null) this.scrollSnapping = listOptions.scrollSnapping;
             if (listOptions.multiselectable != null) this.multiselectable = listOptions.multiselectable;
             if (listOptions.selectionFillColor) this.selectionFillColor = listOptions.selectionFillColor;
+            if (listOptions.selectionTextColor) this.selectionTextColor = listOptions.selectionTextColor;
             if (listOptions.selectionBorderColor) this.selectionBorderColor = listOptions.selectionBorderColor;
             if (listOptions.noSelectOnArrowKey != null) this.noSelectOnArrowKey = listOptions.noSelectOnArrowKey;
             if (listOptions.selectionBorderWidth != null) this.selectionBorderWidth = listOptions.selectionBorderWidth;
+            if (listOptions.secondarySelectionTextColor) this.secondarySelectionTextColor = listOptions.secondarySelectionTextColor;
             if (listOptions.secondarySelectionFillColor) this.secondarySelectionFillColor = listOptions.secondarySelectionFillColor;
             if (listOptions.secondarySelectionBorderColor) this.secondarySelectionBorderColor = listOptions.secondarySelectionBorderColor;
             if (listOptions.initiateSelectionOnArrowKey != null) this.initiateSelectionOnArrowKey = listOptions.initiateSelectionOnArrowKey;
+            if (listOptions.secondarySelectionBorderWidth != null) this.secondarySelectionBorderWidth = listOptions.secondarySelectionBorderWidth;
         }
     }
 }
